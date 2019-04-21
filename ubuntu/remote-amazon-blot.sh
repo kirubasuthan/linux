71ff00d509037e6934fe20da0383b45ff2df9b02
Kiruba Suthan S <kirubasuthan@gmail.com>
2019-04-22 02:32:01 +0530

    Ubuntu remote Amazon blotware

---
+++ ubuntu/remote-amazon-blot.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/Amazon.user.js
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/manifest.json
